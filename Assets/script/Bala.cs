17fd20124c3f013261b89a7ed923cffbda1033a7
AbigailFlorido <A00840336@tec.mx>
2026-04-26 19:11:23 -0600

    balayabalea

---
+++ Assets/script/Bala.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+	[SerializeField] private float velocidad;
+	[SerializeField] private float dano;
+	
+	private void Update()
+	{
+		transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+		Debug.Log ("La bala balea");
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Enemy"))
+		{
+			other.GetComponent<Enemigo>().TomarDano(dano);
+			Destroy(gameObject);
+			Debug.Log ("Daño a enemy");
+		}
+	}
+}

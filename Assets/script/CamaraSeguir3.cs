96c89062fdfb931cd9e0bf317ba5ccbbbbbe013d
Uriel0501 <urielangulo029@gmail.com>
2026-04-29 00:45:03 -0600

    lvl3chingon

---
+++ Assets/script/CamaraSeguir3.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+public class CamaraSeguir3 : MonoBehaviour
+{
+	public Transform jugador;
+	public float speed = 10.0f;
+	public float offsetY = 2.8f;
+	public float offsetX = 3.79f;
+	public float zoom = 3f;
+    
+	//Efecto parallax
+	//Arrastramos los fondos que tengamos
+	public	Transform[] fondos;
+	//valores entre 0 y 1 ( 0 es estatico y 1 se muevve igual que la camara)
+	public float[] velocidadParallax;
+	
+	private	Vector3 posAnteriorCamara;
+	
+	void Start()
+	
+	{
+		posAnteriorCamara = transform.position;
+		Camera.main.orthographicSize = zoom;
+	}
+	
+	
+	void Update()
+	{
+		//Sigue al jugador
+		Vector3 destino = new Vector3 (jugador.position.x + offsetX, jugador.position.y + offsetY, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, destino, speed * Time.deltaTime);
+		//Parallax
+		Vector3 deltaCamara = transform.position - posAnteriorCamara;
+		for (int i = 0; i < fondos.Length; i++) 
+		{
+			float factor = velocidadParallax[i];
+			fondos[i].position += new Vector3(deltaCamara.x * factor, deltaCamara.y * factor, 0);
+		}
+		posAnteriorCamara = transform.position;
+	}
+}

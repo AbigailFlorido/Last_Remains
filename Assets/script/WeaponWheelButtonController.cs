d6f5b2c7870b4557e4eced25b12ec95e86eaca13
AbigailFlorido <A00840336@tec.mx>
2026-04-30 03:56:23 -0600

    yamquierodormir

---
+++ Assets/script/WeaponWheelButtonController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponWheelButtonController : MonoBehaviour
+{
+	public int	ID;
+	private Animator anim;
+	public string itemName;
+	public Image selectedItem;
+	private bool selected = false;
+	public Sprite icon;
+	
+	
+    void Start()
+    {
+	    anim = GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+	    if (selected)
+	    {
+	    	selectedItem.sprite = icon;
+	    }
+    }
+    
+	public void Selected()
+	{
+		selected = true;
+		WeaponWheelController.weaponID = ID;
+	}
+	
+	public void deselected()
+	{
+		selected = false;
+		WeaponWheelController.weaponID = 0;
+	}
+	
+	public void HoverEnter()
+	{
+		anim.SetBool("Hover", true);
+	}
+	
+	public void HoverExit()
+	{
+		anim.SetBool("Hover", false);
+	}
+}
---
+++ Assets/script/WeaponWheelController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponWheelController : MonoBehaviour
+{
+	
+	public Animator anim;
+	private bool weaponWheelSelected = false;
+	public Image selectedItem;
+	public Sprite noImage;
+	public static int weaponID;
+	
+    void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.Tab))
+	    {
+	    	weaponWheelSelected = !weaponWheelSelected;
+	    }
+	    
+	    if (weaponWheelSelected)
+	    {
+	    	anim.SetBool("OpenWeaponWheel", true);
+	    }
+	    
+	    else
+	    {
+	    	anim.SetBool("OpenWeaponWheel", false);
+	    }
+	    
+	    switch(weaponID)
+	    {
+	    	case 1: //hacha
+		    	Debug.Log("Hacha en rueda");
+		    	break;
+	    	case 2: //pistola
+		    	Debug.Log("pistola");
+		    	break;
+	    	case 3: //lanzallamas
+		    	Debug.Log("lanzallamas");
+		    	break;
+	    }
+    }
+}

f8d12c6460f2fc4ef657a241a764c463759ac763
Uriel0501 <urielangulo029@gmail.com>
2026-04-30 06:14:11 -0600

    puch

--- Assets/script/MetaNivel1.cs
+++ Assets/script/MetaNivel1.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MetaNivel : MonoBehaviour
+public class MetaNivel1 : MonoBehaviour
 {
 	public string escenaGanar = "NivelCompletado";
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		// Buscamos si tiene el componente Movimiento en cualquier parte del objeto
-		if (other.GetComponent<Movimiento>() != null || other.GetComponentInParent<Movimiento>() != null)
+		if (other.GetComponent<Movimiento2>() != null || other.GetComponentInParent<Movimiento2>() != null)
 		{
 			Debug.Log("<color=green>¡Meta detectada! Forzando carga de escena: " + escenaGanar + "</color>");
 			
